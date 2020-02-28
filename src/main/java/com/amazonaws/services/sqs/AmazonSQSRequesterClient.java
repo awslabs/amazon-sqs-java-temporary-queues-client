5df0fbecd0cbbca99e45a4b3bdab24343838fcfc
EC2 Default User <ec2-user@ip-172-31-80-215.ec2.internal>
2020-02-28 09:14:50 +0000

    replace src

--- src/main/java/com/amazonaws/services/sqs/AmazonSQSRequesterClient.java
+++ src/main/java/com/amazonaws/services/sqs/AmazonSQSRequesterClient.java
@@ -20,7 +20,7 @@ import com.amazonaws.services.sqs.util.SQSQueueUtils;
 
 /**
  *  Implementation of the request/response interfaces that creates a single
- * temporary queue for each response message.
+ *  temporary queue for each response message.
  */
 class AmazonSQSRequesterClient implements AmazonSQSRequester {
 
